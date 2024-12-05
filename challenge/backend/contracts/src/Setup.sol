8e9a3d94c5b3771d1345bb97de54ddfec650d67b
simonedimaria <117766011+perrythepwner@users.noreply.github.com>
2024-12-05 02:19:54 +0100

    fixed Setup.sol solidity version

--- challenge/backend/contracts/src/Setup.sol
+++ challenge/backend/contracts/src/Setup.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 import { FrontierMarketplace } from "./FrontierMarketplace.sol";
 import { FrontierNFT } from "./FrontierNFT.sol";
