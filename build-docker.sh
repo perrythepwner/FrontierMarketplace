5b7a2abc2ce2e1c4db8e802d80a4f02a47b7771c
simonedimaria <117766011+perrythepwner@users.noreply.github.com>
2024-12-13 14:44:17 +0100

    fixed rpc and env vars

--- build-docker.sh
+++ build-docker.sh
@@ -18,11 +18,5 @@ docker run --rm -it \
     -p "$PUBLIC_RPC_PORT:$LOCAL_RPC_PORT" \
     -p "$FRONTEND_PORT:$FRONTEND_PORT" \
     -p "$HANDLER_PORT:$HANDLER_PORT" \
-    -e PUBLIC_IP=$PUBLIC_IP \
-    -e HANDLER_PORT=$HANDLER_PORT \
-    -e PUBLIC_RPC_PORT=$PUBLIC_RPC_PORT \
-    -e LOCAL_RPC_PORT=$LOCAL_RPC_PORT \
-    -e LOCAL_RPC_URL=$LOCAL_RPC_URL \
-    -e FLAG=$FLAG \
     --name $IMAGE \
     $IMAGE:latest
